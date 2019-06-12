607a54d8eb1476062f62ebfc3adb5404a459f628
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2019-06-12 11:06:38 -0400

    fix: Fix for collection config not found
    The collection config package that is assembled by the endorser removes
    configs for collections in which there was no write set and therefore
    the error 'config for collection [xxx] not found' results. Thes commit
    removes the dependency on the collection config package and uses the
    collection config retriever to manage collection configs.
    
    closes #98
    
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- extensions/endorser/endorser.go
+++ extensions/endorser/endorser.go
@@ -7,12 +7,25 @@ SPDX-License-Identifier: Apache-2.0
 package endorser
 
 import (
-	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/extensions/endorser/api"
 	"github.com/hyperledger/fabric/protos/ledger/rwset"
 )
 
-// FilterPubSimulationResults filters the public simulation results and returns the filtered results or error.
-// The default implementation does not perform any filtering.
-func FilterPubSimulationResults(_ map[string]*common.CollectionConfigPackage, pubSimulationResults *rwset.TxReadWriteSet) (*rwset.TxReadWriteSet, error) {
+// CollRWSetFilter filters out all off-ledger (including transient data) read-write sets from the simulation results
+// so that they won't be included in the block.
+type CollRWSetFilter interface {
+	Filter(channelID string, pubSimulationResults *rwset.TxReadWriteSet) (*rwset.TxReadWriteSet, error)
+}
+
+// NewCollRWSetFilter returns a new collection RW set filter
+func NewCollRWSetFilter(api.QueryExecutorProviderFactory, api.BlockPublisherProvider) CollRWSetFilter {
+	return &collRWSetFilter{}
+}
+
+type collRWSetFilter struct {
+}
+
+// Filter is a noop filter. It simply returns the passed in r/w set
+func (f *collRWSetFilter) Filter(channelID string, pubSimulationResults *rwset.TxReadWriteSet) (*rwset.TxReadWriteSet, error) {
 	return pubSimulationResults, nil
 }
